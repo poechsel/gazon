a6c809e7faf0a24cffeeac4555c2aad57ccc141b
poechsel <pierre.oechsel@gmail.com>
2019-04-22 17:57:45 +0200

    add a small fix to

--- src/common/commands/pass.cpp
+++ src/common/commands/pass.cpp
@@ -6,14 +6,18 @@ public:
 
     void execute(Socket &, Context &context, const CommandArgs &args) {
         std::string pwd = args[0].get<std::string>();
-        if (Config::isUserPwdValid(context.user, pwd)) {
-            context.isLogged = true;
-            context.path = Path("");
+        if (context.user == "") {
+            throw CommandException("Pass must be executed after a call to login");
         } else {
-            context.user = "";
-            context.isLogged = false;
-            context.path = Path("");
-            throw CommandException("Unknown user/pwd pair");
+            if (Config::isUserPwdValid(context.user, pwd)) {
+                context.isLogged = true;
+                context.path = Path("");
+            } else {
+                context.user = "";
+                context.isLogged = false;
+                context.path = Path("");
+                throw CommandException("Unknown user/pwd pair");
+            }
         }
     }
 
